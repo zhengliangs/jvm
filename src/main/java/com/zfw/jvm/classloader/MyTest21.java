80b1ad60141e9a64b8b2d102f50cb02af82bc19c
zhengll <zhengliang166@126.com>
2020-03-29 17:40:53 +0800

    测试不同的类加载器加载同一个类，通过反射赋值

---
+++ src/main/java/com/zfw/jvm/classloader/MyTest21.java
@@ -0,0 +1,30 @@
+package com.zfw.jvm.classloader;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author hero良
+ * @classname MyTest21
+ */
+public class MyTest21 {
+
+    public static void main(String[] args) throws Exception{
+        MyTest16 loader1 = new MyTest16("loader1");
+        MyTest16 loader2 = new MyTest16("loader2");
+
+        loader1.setPath("C:\\Users\\郑良\\Desktop\\");
+        loader2.setPath("C:\\Users\\郑良\\Desktop\\");
+
+        Class<?> clazz1 = loader1.loadClass("com.zfw.jvm.classloader.MyPerson");
+        Class<?> clazz2 = loader2.loadClass("com.zfw.jvm.classloader.MyPerson");
+
+        System.out.println(clazz1 == clazz2);
+
+        Object object1 = clazz1.newInstance();
+        Object object2 = clazz2.newInstance();
+
+        Method method = clazz1.getMethod("setMyPerson", Object.class);
+        method.invoke(object1, object2);
+        System.out.println("---------");
+    }
+}
