a1b90513436320f4b9eb841b6937aa6f42f51219
zhengll <zhengliang166@126.com>
2020-04-08 21:14:37 +0800

    字节码篇

---
+++ src/main/java/com/zfw/jvm/bytecode/MyTest1.java
@@ -0,0 +1,18 @@
+package com.zfw.jvm.bytecode;
+
+/**
+ * @author hero良
+ * @classname MyTest1
+ */
+public class MyTest1 {
+
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
