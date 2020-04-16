61f97e3c190963d18ee113521ea11ce3cf0c8028
zhengliang <zhengliang166@126.com>
2020-04-16 23:31:01 +0800

    GC测试

--- src/main/java/com/zfw/jvm/gc/MyTest5.java
+++ src/main/java/com/zfw/jvm/gc/MyTest5.java
@@ -10,20 +10,22 @@ public class MyTest5 {
     public static void main(String[] args) {
         int size = 1024 * 1024;
 
-        byte[] myAlloc1 = new byte[3 * size];
+        System.out.println(MyTest6.by);
 
-        System.out.println("11111");
-
-        byte[] myAlloc2 = new byte[4 * size];
-
-        System.out.println("22222");
-
-        byte[] myAlloc3 = new byte[3 * size];
-
-        System.out.println("33333");
-
-        byte[] myAlloc4 = new byte[3 * size];
-
-        System.out.println("44444");
+//        byte[] myAlloc1 = new byte[3 * size];
+//
+//        System.out.println("11111");
+//
+//        byte[] myAlloc2 = new byte[4 * size];
+//
+//        System.out.println("22222");
+//
+//        byte[] myAlloc3 = new byte[3 * size];
+//
+//        System.out.println("33333");
+//
+//        byte[] myAlloc4 = new byte[3 * size];
+//
+//        System.out.println("44444");
     }
 }
---
+++ src/main/java/com/zfw/jvm/gc/MyTest6.java
@@ -0,0 +1,10 @@
+package com.zfw.jvm.gc;
+
+/**
+ * @author hero良
+ * @classname MyTest6
+ */
+public class MyTest6 {
+
+    public static final byte[] by = new byte[1024 * 1024 * 9];
+}
