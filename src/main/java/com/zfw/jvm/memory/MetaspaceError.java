9fa2ce235c4ad54d252d388529fee0894b1b1673
尼古拉斯·编程小学生 <44744090+zhengliangs@users.noreply.github.com>
2020-04-23 09:22:46 +0800

    测试元空间溢出

--- src/main/java/com/zfw/jvm/memory/MetaspaceError.java
+++ src/main/java/com/zfw/jvm/memory/MetaspaceError.java
@@ -4,16 +4,19 @@ import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 
 /**
+ * -XX:MetaspaceSize=5m -XX:MaxMetaspaceSize=10m
+ * java.lang.OutOfMemoryError: Metaspace
+ * 运行期间不停的动态生成Class，最后会导致元空间的溢出
  * @author hero良
- * @classname MyTest4
+ * @classname MetaspaceError
  */
-public class MyTest4 {
+public class MetaspaceError {
 
     //运行期生成Class对象，模拟元空间的内存溢出
     public static void main(String[] args) {
         for (; ; ){
             Enhancer enhancer = new Enhancer();
-            enhancer.setSuperclass(MyTest4.class);
+            enhancer.setSuperclass(MetaspaceError.class);
             enhancer.setUseCache(false);
             enhancer.setCallback((MethodInterceptor)(obj, method, args1, proxy) -> proxy.invokeSuper(obj, args1));
 
