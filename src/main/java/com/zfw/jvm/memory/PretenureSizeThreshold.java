ca598ae700830a4458942f5c02e2062f5b5b2bc2
zhengliang <zhengliang166@126.com>
2020-08-27 23:59:26 +0800

    GC测试

--- src/main/java/com/zfw/jvm/memory/PretenureSizeThreshold.java
+++ src/main/java/com/zfw/jvm/memory/PretenureSizeThreshold.java
@@ -1,10 +1,10 @@
-package com.zfw.jvm.gc;
+package com.zfw.jvm.memory;
 
 /**
  * 新分配的对象如果大小超过PretenureSizeThreshold设置的值，则直接分配在老年代
  *
- * -verbose:gc -Xmx40m -Xms40m -Xmn20m -XX:SurvivorRatio=8 -XX:+PrintGCDetails -XX:+PrintCommandLineFlags
- * -XX:PretenureSizeThreshold=4194304
+ * -verbose:gc -Xmx40m -Xms40m -Xmn20m -XX:SurvivorRatio=8
+ * -XX:PretenureSizeThreshold=4194304 -XX:+UseSerialGC -XX:+PrintGCDetails
  *
  * PretenureSizeThreshold 只在UseSerialGC和ConcMarkSweepGC收集器下有效
  *
@@ -16,12 +16,6 @@ public class PretenureSizeThreshold {
     public static void main(String[] args) {
         int size = 1024 * 1024;
 
-        byte[] myAlloc = new byte[6 * size];
-
-//        try {
-//            Thread.sleep(1000000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        byte[] allocation = new byte[6 * size];
     }
 }
